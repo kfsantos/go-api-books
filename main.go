47367ebf3fb9a514e92a203c3cdaefc369cacbbc
klinger dos santos <kfsantos@gmail.com>
2022-01-24 15:28:35 -0300

    Update main.go
    Carregando .env

--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"log"
 
+	"github.com/joho/godotenv"
 	"github.com/kfsantos/books/database"
 	"github.com/kfsantos/books/routers"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
 	database.Setup()
 	r := routers.Setup()
 	if err := r.Run("127.0.0.1:5000"); err != nil {
